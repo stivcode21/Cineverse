237afe8b7912e692ba59eba099f4927d0f6dfb88
stivcode21 <stivenelcodificador@gmail.com>
2025-08-15 22:49:32 -0500

    feat: new styles filter

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -5,12 +5,14 @@ const Filter = ({ selectedTag, setSelectedTag, role }) => {
       : ["Todas", "Comedia", "Ficcion", "Misterio", "Infantil"];
 
   return (
-    <div className="flex justify-start items-center py-2 pl-4 md:pl-14 bg-black">
+    <div className="flex flex-col justify-center items-center py-2">
       {Tags.map((tag) => (
         <button
           key={tag}
-          className={`mx-2 p-2 text-[12px] md:text-[16px] rounded-sm font-medium hover:bg-blue-500 ${
-            selectedTag === tag ? "bg-blue-500" : "bg-gray-900"
+          className={`mx-2 p-2 text-[12px] md:text-[16px] rounded-sm font-medium hover:text-[#00B3FA] hover:scale-105 transition-all duration-[0.3ms] ease-in-out ${
+            selectedTag === tag
+              ? "text-[#00B3FA] hover:scale-100"
+              : "text-white"
           } text-white`}
           onClick={() => {
             setSelectedTag(tag); // Actualiza la etiqueta seleccionada al hacer clic

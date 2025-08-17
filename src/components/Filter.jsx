951708cdb70e8e66d92588640874554fd7d4654b
stivcode21 <stivenelcodificador@gmail.com>
2025-08-17 08:28:42 -0500

    style: changes in colors and actions in filter

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,25 +1,35 @@
-const Filter = ({ selectedTag, setSelectedTag, role }) => {
+const Filter = ({ selectedTag, setSelectedTag, role, closeList }) => {
   const Tags =
     role === "movie"
       ? ["Todas", "Terror", "Comedia", "Romance", "Infantil"]
       : ["Todas", "Comedia", "Ficcion", "Misterio", "Infantil"];
 
+  const handleTag = (tag) => {
+    setSelectedTag(tag);
+    closeList(false);
+  };
+
   return (
-    <div className="flex flex-col justify-center items-center py-2">
+    <div className="flex flex-col justify-center items-center gap-2 pb-2">
       {Tags.map((tag) => (
-        <button
-          key={tag}
-          className={`mx-2 p-2 text-[12px] md:text-[16px] rounded-sm font-medium hover:text-[#00B3FA] hover:scale-105 transition-all duration-[0.3ms] ease-in-out ${
-            selectedTag === tag
-              ? "text-[#00B3FA] hover:scale-100"
-              : "text-white"
-          } text-white`}
-          onClick={() => {
-            setSelectedTag(tag); // Actualiza la etiqueta seleccionada al hacer clic
-          }}
-        >
-          {tag}
-        </button>
+        <div className="group">
+          <button
+            key={tag}
+            className={` my-[1px] text-[12px] md:text-[14px] rounded-sm font-medium uppercase hover:text-[#00B3FA] hover:scale-105 transition-all duration-[0.3ms] ease-in-out ${
+              selectedTag === tag
+                ? "text-[#00B3FA] hover:scale-[1]"
+                : "text-white"
+            }`}
+            onClick={() => handleTag(tag)}
+          >
+            {tag}
+          </button>
+          {selectedTag === tag ? (
+            <span className="mx-auto w-6 h-[0.5px] bg-[#00B3FA] block transition-all ease-in-out"></span>
+          ) : (
+            ""
+          )}
+        </div>
       ))}
     </div>
   );
